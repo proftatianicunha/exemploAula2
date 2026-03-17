e05d5df52220b2ae8dcbc719aa625f1423c78e57
Tatiani Cunha <tatianicpsantana@gmail.com>
2026-03-17 15:57:50 -0300

    Initial commit

---
+++ app/src/main/java/com/example/exemploaula2/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.exemploaula2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, ExemploFragment())
+                .commit()
+        }
+    }
+}
